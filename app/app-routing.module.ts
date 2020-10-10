033d171fa8abf9b1e8a5738f7acae260985c63a9
oletendart <letendartolivia@gmail.com>
2020-10-10 11:59:48 +0200

    Create const routes contains array contains different path in the file app-routing.module.ts

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -1,6 +1,12 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'fulll' },
+  
+]
+
+
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules})
