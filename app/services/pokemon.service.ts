e230f10bd48dcce3a744e4cb93e5fb0cde48820e
oletendart <letendartolivia@gmail.com>
2020-10-10 13:37:27 +0200

    create array function in the file pokemon.service.ts

--- app/services/pokemon.service.ts
+++ app/services/pokemon.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 @Injectable({
  providedIn: 'root'
@@ -12,6 +13,10 @@ export class PokemonService {
   constructor(private http: HttpClient) {}
 
   getPokemon(offset = 0) {
-    return this.http.get(`${this.baseUrl}/pokemon?offset=${offset}&limit=25`):
+    return this.http.get(`${this.baseUrl}/pokemon?offset=${offset}&limit=25`).pipe(
+      map( result => {
+        return result['results'];
+      })
+    )
   }
 }
