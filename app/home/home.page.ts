e1f440b9e461bb225de764e3d314abd673a55506
oletendart <letendartolivia@gmail.com>
2020-10-10 14:02:32 +0200

    Create details.module.ts + create news function onSearchChange in the file home.page.ts

--- app/home/home.page.ts
+++ app/home/home.page.ts
@@ -1,26 +1,57 @@
-import { Component, OnInit } from '@angular/core';
-import { PokemonService } from '../services/pokemon.service';
-
-
+import { PokemonService } from './../services/pokemon.service';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonInfiniteScroll } from '@ionic/angular';
+ 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.css'],
+  styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
   offset = 0;
   pokemon = [];
-
-  contructor(private pokeService: PokemonService) {}
-
-  ngOnInit() {
+ 
+  @ViewChild(IonInfiniteScroll) infinite: IonInfiniteScroll;
+ 
+  constructor(private pokeService: PokemonService) { }
+ 
+  ngOnInit()  {
     this.loadPokemon();
   }
-
-  loadPokemon() {
-    this.pokeService.getPokemon(this.offset).suscribe(res => {
-      console.log('result: ', res);
-      this.pokemon = res;
-    })
+ 
+  loadPokemon(loadMore = false, event?) {
+    if (loadMore) {
+      this.offset += 25;
+    }
+ 
+    this.pokeService.getPokemon(this.offset).subscribe(res => {
+      this.pokemon = [...this.pokemon, ...res];
+ 
+      if (event) {
+        event.target.complete();
+      }
+ 
+      // Optional
+      if (this.offset == 125) {
+        this.infinite.disabled = true;
+      }
+    });
   }
+ 
+  onSearchChange(e) {
+    let value = e.detail.value;
+ 
+    if (value == '') {
+      this.offset = 0;
+      this.loadPokemon();
+      return;
+    }
+ 
+    this.pokeService.findPokemon(value).subscribe(res => {
+      this.pokemon = [res];
+    }, err => {
+      this.pokemon = [];
+    });
+  }
+}
 }
