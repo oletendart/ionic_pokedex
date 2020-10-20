ec0e567ad1dd45956b828fd7b5ca7b5b7c60b3d7
oletendart <letendartolivia@gmail.com>
2020-10-20 17:18:27 +0200

    Install packages in the application

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,19 +1,16 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
-
-import { TabsPage } from '../pages/tabs/tabs';
+import { Component } from "@angular/core";
+import { Platform } from "ionic-angular";
 
 @Component({
-  templateUrl: 'app.html'
+  templateUrl: "app.html"
 })
 export class MyApp {
-  rootPage:any = TabsPage;
+  rootPage: any;
 
   constructor(platform: Platform) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-
     });
   }
 }
